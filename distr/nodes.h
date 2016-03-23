54f3191cf3ef58303cb8866c53512889818cb060
Jamis <hoojamis@gmail.com>
2016-03-23 12:22:19 +0800

    set buffer size 512MB

--- distr/nodes.h
+++ distr/nodes.h
@@ -3,7 +3,7 @@
 
 #include "ec/ec-method.h"
 
-#define NUM_CLIENTS (2)
+#define NUM_CLIENTS (4)
 
 const int server_port = 5000;
 const char server_port_string[] = "5000";
@@ -11,7 +11,7 @@ const char server_ip[] = "192.168.50.1";
 
 #define RESOLVE_TIMEOUT_MS (5000)
 #define BUFFER_HEADER_SIZE (1024)
-#define BUFFER_BODY_SIZE (1L << 27) // maximum allowed value is 512MB
+#define BUFFER_BODY_SIZE (1L << 29) // maximum allowed value is 512MB
 #define BUFFER_SIZE (BUFFER_BODY_SIZE + BUFFER_HEADER_SIZE)
 #define COLUMN (4)
 #define ROW (COLUMN / 4 * 5)
