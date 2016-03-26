864cee87cfff0fa64b2fbd06f47128a44c1f3d60
Jamis <hoojamis@gmail.com>
2016-03-26 16:36:59 +0800

    add decode constants

--- distr/nodes.h
+++ distr/nodes.h
@@ -3,20 +3,24 @@
 
 #include "ec/ec-method.h"
 
-#define NUM_CLIENTS (4)
+#define NUM_CLIENTS (2)
+
+#define COLUMN (4)
+#define ROW (COLUMN + 2)
+#define NUM_BRICKS (ROW)
 
 const int server_port = 5000;
 const char server_port_string[] = "5000";
 const char server_ip[] = "192.168.50.1";
 
 #define RESOLVE_TIMEOUT_MS (5000)
-#define BUFFER_HEADER_SIZE (1024)
-#define BUFFER_BODY_SIZE (1L << 29) // maximum allowed value is 512MB
-#define BUFFER_SIZE (BUFFER_BODY_SIZE + BUFFER_HEADER_SIZE)
-#define COLUMN (4)
-#define ROW (COLUMN / 4 * 5)
 
-#define NUM_BRICKS (ROW)
+#define BUFFER_HEADER_SIZE (1024)
+#define BUFFER_BODY_SIZE (1L << 29)
+#define BUFFER_SIZE (BUFFER_BODY_SIZE + BUFFER_HEADER_SIZE)
+#define DECODE_BUFFER_BODY_SIZE (BUFFER_BODY_SIZE / COLUMN * ROW)
+#define DECODE_BUFFER_SIZE (DECODE_BUFFER_BODY_SIZE + BUFFER_HEADER_SIZE)
+// maximum allowed value of DECODE_BUFFER_SIZE is 1GiB
 
 #define ENCODE_BLOCK_SIZE (EC_METHOD_CHUNK_SIZE * COLUMN) 
 
