947682f74898976861a4b3a4a63580f5a3e3e015
Jamis <hoojamis@gmail.com>
2016-04-08 19:48:15 +0800

    add ec-method header

---
+++ systematic-crs/ec-method.h
@@ -0,0 +1,25 @@
+#ifndef __EC_METHOD_H__
+#define __EC_METHOD_H__
+
+#include <inttypes.h>
+#include <stddef.h>
+
+#define EC_GF_BITS 8
+#define EC_GF_MOD 0x11D
+#define EC_GF_SIZE (1 << EC_GF_BITS)
+#define EC_GF_WORD_SIZE sizeof(uint64_t)
+
+#define EC_METHOD_MAX_FRAGMENTS 16 // may be larger?
+#define EC_METHOD_MAX_NODES (EC_GF_SIZE - 1)
+#define EC_METHOD_WORD_SIZE 64 // delete if not used
+#define EC_METHOD_CHUNK_SIZE (EC_METHOD_WORD_SIZE * EC_GF_BITS)
+#define EC_METHOD_WIDTH (EC_METHOD_WORD_SIZE / EC_GF_WORD_SIZE) // delete if not used
+
+
+void ec_method_initialize(void);
+size_t ec_method_encode(size_t size, uint32_t columns, uint32_t row,
+                        uint8_t * in, uint8_t * out);
+size_t ec_method_decode(size_t size, uint32_t columns, uint32_t * rows,
+                        size_t n_recv, uint8_t ** in, uint8_t * out);
+
+#endif /* __EC_METHOD_H__ */
