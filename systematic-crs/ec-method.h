c0134fefcb3ebcd67acaf6fb359dadc21d60f604
Jamis <hoojamis@gmail.com>
2016-04-10 20:59:10 +0800

    remove outdated comment

--- systematic-crs/ec-method.h
+++ systematic-crs/ec-method.h
@@ -9,11 +9,11 @@
 #define EC_GF_SIZE (1 << EC_GF_BITS)
 #define EC_GF_WORD_SIZE sizeof(uint64_t)
 
-#define EC_METHOD_MAX_FRAGMENTS 256 // may be larger?
+#define EC_METHOD_MAX_FRAGMENTS 256
 #define EC_METHOD_MAX_NODES (EC_GF_SIZE - 1)
-#define EC_METHOD_WORD_SIZE 64 // delete if not used
+#define EC_METHOD_WORD_SIZE 64
 #define EC_METHOD_CHUNK_SIZE (EC_METHOD_WORD_SIZE * EC_GF_BITS)
-#define EC_METHOD_WIDTH (EC_METHOD_WORD_SIZE / EC_GF_WORD_SIZE) // delete if not used
+#define EC_METHOD_WIDTH (EC_METHOD_WORD_SIZE / EC_GF_WORD_SIZE)
 
 
 void ec_method_initialize(void);
