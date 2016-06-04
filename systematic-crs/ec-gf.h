272ffc947b8b9307135f6623945feb41fb9d74f3
Jamis <hoojamis@gmail.com>
2016-06-04 16:21:33 +0800

    use AVX2

--- systematic-crs/ec-gf.h
+++ systematic-crs/ec-gf.h
@@ -11,14 +11,14 @@
 #define encode_t __m256
 
 #define XOR1(A) (A)
-#define XOR2(A,B) _mm256_xor_ps(A,B)
-#define XOR3(A,B,C) _mm256_xor_ps(_mm256_xor_ps(A,B),C)
-#define XOR4(A,B,C,D) _mm256_xor_ps(_mm256_xor_ps(_mm256_xor_ps(A,B),C),D)
-#define XOR5(A,B,C,D,E) _mm256_xor_ps(_mm256_xor_ps(_mm256_xor_ps(_mm256_xor_ps(A,B),C),D),E)
-#define XOR6(A,B,C,D,E,F) _mm256_xor_ps(_mm256_xor_ps(_mm256_xor_ps(_mm256_xor_ps(_mm256_xor_ps(A,B),C),D),E),F)
-#define XOR7(A,B,C,D,E,F,G) _mm256_xor_ps(_mm256_xor_ps(_mm256_xor_ps(_mm256_xor_ps(_mm256_xor_ps(_mm256_xor_ps(A,B),C),D),E),F),G)
-#define XOR8(A,B,C,D,E,F,G,H) _mm256_xor_ps(_mm256_xor_ps(_mm256_xor_ps(_mm256_xor_ps(_mm256_xor_ps(_mm256_xor_ps(_mm256_xor_ps(A,B),C),D),E),F),G),H)
-#define XOR9(A,B,C,D,E,F,G,H,I) _mm256_xor_ps(_mm256_xor_ps(_mm256_xor_ps(_mm256_xor_ps(_mm256_xor_ps(_mm256_xor_ps(_mm256_xor_ps(_mm256_xor_ps(A,B),C),D),E),F),G),H),I)
+#define XOR2(A,B) _mm256_xor_si256(A,B)
+#define XOR3(A,B,C) _mm256_xor_si256(_mm256_xor_si256(A,B),C)
+#define XOR4(A,B,C,D) _mm256_xor_si256(_mm256_xor_si256(_mm256_xor_si256(A,B),C),D)
+#define XOR5(A,B,C,D,E) _mm256_xor_si256(_mm256_xor_si256(_mm256_xor_si256(_mm256_xor_si256(A,B),C),D),E)
+#define XOR6(A,B,C,D,E,F) _mm256_xor_si256(_mm256_xor_si256(_mm256_xor_si256(_mm256_xor_si256(_mm256_xor_si256(A,B),C),D),E),F)
+#define XOR7(A,B,C,D,E,F,G) _mm256_xor_si256(_mm256_xor_si256(_mm256_xor_si256(_mm256_xor_si256(_mm256_xor_si256(_mm256_xor_si256(A,B),C),D),E),F),G)
+#define XOR8(A,B,C,D,E,F,G,H) _mm256_xor_si256(_mm256_xor_si256(_mm256_xor_si256(_mm256_xor_si256(_mm256_xor_si256(_mm256_xor_si256(_mm256_xor_si256(A,B),C),D),E),F),G),H)
+#define XOR9(A,B,C,D,E,F,G,H,I) _mm256_xor_si256(_mm256_xor_si256(_mm256_xor_si256(_mm256_xor_si256(_mm256_xor_si256(_mm256_xor_si256(_mm256_xor_si256(_mm256_xor_si256(A,B),C),D),E),F),G),H),I)
 
 #else
 
